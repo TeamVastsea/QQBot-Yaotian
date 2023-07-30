1b1aeb4206961480e322af94e684c827d019212f
rabbit0w0 <rabbit0w0@outlook.com>
2023-07-30 09:30:26 +0800

    completed blocklist

--- YaoTian/Modules/BlockList.cs
+++ YaoTian/Modules/BlockList.cs
@@ -4,6 +4,7 @@ using Konata.Core.Message;
 using MongoDB.Driver;
 using MongoDB.Bson;
 using YaoTian.ModuleMgr;
+using System.Text;
 
 namespace YaoTian.Modules;
 
@@ -44,7 +45,7 @@ internal class BlockList : ModuleBase
                 { "QQCode", qq },
                 { "Reason", reason },
                 { "Source", source },
-                { "Time", msg.EventTime.AddHours(8) }
+                { "Time", msg.EventTime.ToLocalTime().ToString("MM/dd/yyyy HH:mm:ss") + TimeZoneInfo.Local.ToString() }
             };
             documents.Add(document);
         }
@@ -52,44 +53,46 @@ internal class BlockList : ModuleBase
         collection.InsertMany(documents);
         return new MessageBuilder().Text($"已将{documents.Count}个QQ加入黑名单");
     }
-    
-    
-    // [Command("blockquery",
-    //     Name = "BlockQuery",
-    //     Alias = new[] { "banquery", "banq", "blockq", "查黑" },
-    //     Permission = PermissionLevel.Admin,
-    //     Matching = Matching.StartsWithLeadChar,
-    //     Description = "Add a user to the block list")]
-    // public static MessageBuilder OnCommandBlockQuery(Bot bot, GroupMessageEvent msg)
-    // {
-    //     var args = msg.ParseArgListFromMsgEvent();
-    //     if (args.Count < 1)
-    //         return new MessageBuilder().Text("指令格式: /blockq <qqCode> <reason>");
-    //     
-    //     var mongoUrl = BotApp.Config.MongoUrl;
-    //     var client = new MongoClient(mongoUrl);
-    //     var database = client.GetDatabase("vastsea");
-    //     var collection = database.GetCollection<BsonDocument>("blockList");
-    //     
-    //     var source = $"{msg.GroupName}[{msg.GroupUin}] - {msg.MemberCard}[{msg.MemberUin}]";
-    //     var qqList = args[0].Replace("，", ",").Split(',');
-    //     var reason = string.Join(" ", args.Skip(1));
-    //     
-    //     var documents = new List<BsonDocument>();
-    //
-    //     foreach (var qq in qqList)
-    //     {
-    //         var document = new BsonDocument()
-    //         {
-    //             { "QQCode", qq },
-    //             { "Reason", reason },
-    //             { "Source", source },
-    //             { "Time", msg.EventTime }
-    //         };
-    //         documents.Add(document);
-    //     }
-    //     
-    //     collection.InsertMany(documents);
-    //     return new MessageBuilder().Text($"已将{documents.Count}个QQ加入黑名单");
-    // }
+
+
+    [Command("blockquery",
+        Name = "BlockQuery",
+        Alias = new[] { "query", "查黑" },
+        Permission = PermissionLevel.Admin,
+        Matching = Matching.StartsWithLeadChar,
+        Description = "Query a user's blocklist record")]
+    public static MessageBuilder OnCommandBlockQuery(Bot bot, GroupMessageEvent msg)
+    {
+        var args = msg.ParseArgListFromMsgEvent();
+        if (args.Count < 1)
+            return new MessageBuilder().Text("指令格式: /query <qqCode>");
+
+        var mongoUrl = BotApp.Config.MongoUrl;
+        var client = new MongoClient(mongoUrl);
+        var database = client.GetDatabase("vastsea");
+        var collection = database.GetCollection<BsonDocument>("blockList");
+
+        var qq = args[0];
+
+        var filter = Builders<BsonDocument>.Filter.Eq("QQCode", qq);
+        var resultList = collection.Find(filter).ToList();
+        
+        if (resultList.Count == 0)
+        {
+            // No any record
+            return new MessageBuilder().Text("该QQ没有任何黑名单记录！");
+        }
+        else
+        {
+            StringBuilder builder = new();
+            resultList.ForEach((item) => {
+                var time = item.GetValue("Time").AsString;
+                var soure = item.GetValue("Source").AsString;
+                var reason = item.GetValue("Reason").AsString;
+
+                builder.Append($"时间: {time}\n原因: {reason}\n");
+            });
+            return new MessageBuilder().Text(builder.ToString());
+        }
+    }
 }
